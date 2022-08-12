06c6547f3d210dfed1885c1d0f401defb350659e
LordOfPolls <ddavidallen13@gmail.com>
2022-08-12 22:22:24 +0100

    feat: add track decoding support

--- naff_link/models/track.py
+++ naff_link/models/track.py
@@ -1,3 +1,7 @@
+import struct
+from base64 import b64decode
+from io import BytesIO
+
 from attr import define, field
 
 from naff_link import get_logger
@@ -51,6 +55,48 @@ class Track:
             )
         except KeyError as e:
             log.error(f"Missing key in track data: {e} :: {data}")
+
+    @classmethod
+    def from_encode(cls, encoded: str) -> "Track":
+        data = BytesIO(b64decode(encoded))
+
+        flags = (struct.unpack(">i", data.read(4))[0] & 0xC0000000) >> 30
+        # version data we dont care about
+        _ = struct.unpack("B", data.read(1)) if flags & 1 != 0 else 1
+
+        title_length = struct.unpack(">H", data.read(2))[0]
+        title = data.read(title_length).decode(errors="ignore")
+
+        author_length = struct.unpack(">H", data.read(2))[0]
+        author = data.read(author_length).decode(errors="ignore")
+
+        track_length = struct.unpack(">Q", data.read(8))[0]
+
+        identifier_length = struct.unpack(">H", data.read(2))[0]
+        identifier = data.read(identifier_length).decode(errors="ignore")
+
+        is_stream = bool(struct.unpack("B", data.read(1))[0])
+
+        if bool(struct.unpack("B", data.read(1))[0]):
+            uri_length = struct.unpack(">H", data.read(2))[0]
+            uri = data.read(uri_length).decode(errors="ignore")
+
+        source_length = struct.unpack(">H", data.read(2))[0]
+        source = data.read(source_length).decode(errors="ignore")
+        position = struct.unpack(">Q", data.read(8))[0]
+
+        return cls(
+            encoded=encoded,
+            uri=uri,
+            identifier=identifier,
+            title=title,
+            author=author,
+            source_name=source,
+            is_seekable=not is_stream,
+            is_stream=is_stream,
+            position=position,
+            length=track_length,
+        )
 
     def __str__(self) -> str:
         return self.encoded
