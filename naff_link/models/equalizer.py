f4ee479191b26774f1d1a38807e39bd44b447f16
LordOfPolls <ddavidallen13@gmail.com>
2022-08-16 22:19:53 +0100

    fix: missing band

--- naff_link/models/equalizer.py
+++ naff_link/models/equalizer.py
@@ -76,7 +76,7 @@ class Equalizer:
     def piano(cls) -> "Equalizer":
         """Create a piano equalizer"""
         eq = cls()
-        eq.set_bands([-0.25, -0.25, -0.125, 0.0, 0.25, 0.25, 0.0, -0.25, -0.25, 0.0, 0.0, 0.5, 0.25, -0.025])
+        eq.set_bands([-0.25, -0.25, -0.25, -0.125, 0.0, 0.25, 0.25, 0.0, -0.25, -0.25, 0.0, 0.0, 0.5, 0.25, -0.025])
         return eq
 
     @classmethod
